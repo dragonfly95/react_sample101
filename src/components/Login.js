47aa9aba7b098881bb87509427d10cd696f521fd
dragonfly95 <dQzNKCyAgf43e3B>
2021-02-19 14:00:25 +0900

    cvb

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,43 +1,49 @@
-import {inject, observer} from "mobx-react";
-import * as React from "react";
-import {action, observable} from "mobx";
+import React, { Component } from 'react';
+import { observable, action } from 'mobx'
+import { observer } from 'mobx-react'
+
+// https://medium.com/@jsh901220/mobx-%EC%B2%98%EC%9D%8C-%EC%8B%9C%EC%9E%91%ED%95%B4%EB%B3%B4%EA%B8%B0-a768f4aaa73e
 
 @observer
 class Login extends React.Component {
-    @observable email = '';
-    @observable passwords = '';
+    // @observable emails = '';
+    // @observable passwords = '';
 
     constructor(props) {
-        super(props);
-        this.onSubmit = this.onSubmit.bind(this);
+        super(props)
+        this.onSubmit = this.onSubmit.bind(this)
+        this.onChange = this.onChange.bind(this)
     }
 
-    render(){
-        const { email, password } = this;
+    render() {
+        const { email, password } = this
+
         return (
-            <div>
+            <div className="App">
                 <header>Login</header>
-                <input name="email" placeholder="Email" onChange={this.onChange}
-                       value={email} fluid/>
-               <input name="password" type="password" placeholder="Password"
-                      onChange={this.onChange} value={password} fluid/>
-               <button onClick={this.onSubmit}> submit </button>
-
-
+                <input name="email" placeholder="Email" onChange={this.onChange} value={email} fluid />
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    onChange={this.onChange}
+                    value={password}
+                    fluid
+                />
+                <button onClick={this.onSubmit}>Submit</button>
             </div>
         );
     }
 
-    @action.bound
-    onChange(event) {
+
+    onChange (event) {
         const { name, value } = event.target;
-        debugger
         this[name] = value;
     }
 
     onSubmit() {
-        const { email, password } = this;
-        console.log('결과확인: ', email, password);
+        const { email, password } = this
+        console.log('결과확인 : ', email, password);
     }
 }
 
